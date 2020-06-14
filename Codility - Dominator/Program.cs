74cd7f94bb43b570d424b28e4d5a0ffcbd055e84
Stefan Alexandru <stefanalexandru02@outlook.com>
2020-06-14 11:57:53 +0300

    solution 100/100

---
+++ Codility - Dominator/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Codility___Dominator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        /// Dominator - 100/100
+        public int solution(int[] A)
+        {
+            Dictionary<int, int> numsCout = new Dictionary<int, int>();
+            int maxV = 0;
+            int maxI = 0;
+            int index = 0;
+            foreach(var i in A)
+            {
+                if (numsCout.ContainsKey(i)) numsCout[i]++;
+                else numsCout.Add(i, 1);
+
+                if(numsCout[i] > maxV)
+                {
+                    maxV = numsCout[i];
+                    maxI = index;
+                }
+
+                index++;
+            }
+
+            if (maxV > A.Length / 2)
+                return maxI;
+            return -1;
+        }
+    }
+}
---
+++ Codility - DominatorTests/ProgramTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Codility___Dominator;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Codility___Dominator.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void solutionTest()
+        {
+            Program p = new Program();
+            int[] A = new int[8];
+            A[0] = 3; 
+            A[1] = 4;    
+            A[2] = 3;
+            A[3] = 2;
+            A[4] = 3; 
+            A[5] = -1;
+            A[6] = 3;
+            A[7] = 3;
+            int output = p.solution(A);
+            Assert.IsTrue(output == 0 || output == 2 || output == 4 || output == 6 || output == 7);
+        }
+    }
+}
